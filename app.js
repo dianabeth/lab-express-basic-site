722f03d75dc5f4d09a8598c4358c07a85c367654
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-07-21 23:50:44 +0100

    done

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/home', (request, response) => {
-  response.sendFile(__dirname + '/views/home.html');
-});
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
-app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+hbs.registerPartials(__dirname + '/views/partials');
+app.locals.title = 'Default title...';
+
+app.get('/home', (request, response) => {
+  response.render('main', { title: 'Home', message: 'This is the home page' });
+  //response.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/work', (request, response) => {
-  response.sendFile(__dirname + '/views/work.html');
+  response.render('main', { title: 'Work', message: 'Display content about her career here!' });
+  //response.sendFile(__dirname + '/views/work.html');
 });
 
 app.get('/gallery', (request, response) => {
-  response.sendFile(__dirname + '/views/gallery.html');
+  response.render('main', { title: 'Gallery', message: 'Display some images here!' });
+  //response.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.get('/about', (request, response) => {
+  response.render('about', {
+    name: 'Ngozi Okonjo-Iweala',
+    age: 66,
+    location: { city: 'New York', country: 'USA' },
+    husband: 'Dr. Ikemba Iweala',
+    children: [
+      { name: 'Okechukwu Iweala', isMale: true, graduated: 'Harvard' },
+      { name: 'Uzodinma Iweala', isMale: true, graduated: 'Harvard' },
+      { name: 'Onyinye Iweala', isMale: false, graduated: 'Harvard' },
+      { name: 'Uchechi Iweala', isMale: true, graduated: 'Harvard' }
+    ]
+  });
 });
 
 app.get('*', (request, response) => {
-  console.log('Request received');
+  //console.log('Request received');
   response.send('Hello, Hello, Hello');
 });
 
